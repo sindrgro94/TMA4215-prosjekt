95121463191154f3f1773f4bb90712f3b48a35dd
Tor Erik Larsen <torlarse@stud.ntnu.no>
2016-11-10 14:28:08 +0100

    commit fra salmaskin

--- onestep_solver.m
+++ onestep_solver.m
@@ -1,7 +1,14 @@
 %% Introduction
 % This script answers task 8
+% For each iteration, calls onestep.m to solve one step of a test problem
 %' @jac jacobi matrix
 
+%% initializing test functions
+% All test problems are formatted {function, Jacobian, current time, .....
+% current solution value, stepsize and 
+
 jac = jacobi('test')
+
+f = @(t,y) [t - 2*y(1) + y(2);t + y(1)- 2*y(2) + 3];
 
 [tnext, ynext, le, iflag] = onestep(f,jac,tn,yn,h,Tolit)
