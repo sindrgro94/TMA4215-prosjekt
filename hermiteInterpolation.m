b53562946f4116d9ffadeea48fcebb03b6697a49
sindrgro94 <sindre.grostad@icloud.com>
2016-11-17 19:29:02 +0100

    interpolasjon og ball fungerer

--- hermiteInterpolation.m
+++ hermiteInterpolation.m
@@ -1,4 +1,18 @@
-function [tImpact,yImpact,speedImpact] = hermiteInterpolation(t,y)
-
-
+function [tEvent,yEvent] = hermiteInterpolation(t,y)
+    h = t(2)-t(1);
+    P0 = y(1,1);
+    P3 = y(1,2);
+    P1 = P0+h*y(2,1)/3;
+    P2 = P3-h*y(2,2)/3;
+    B = @(theta) P0*(1-theta)^3+3*P1*(1-theta)^2*theta+...
+        3*P2*(1-theta)*theta^2+P3*theta^3;
+    Bder = @(theta) -3*P0*(1-theta)^2+3*P3*theta^2+...
+        3*P1*((1-theta)^2-2*theta*(1-theta))+3*P2*(2*(1-theta)*theta-theta^2);
+    %Newtons method
+    thetaN = 0.5;
+    while abs(B(thetaN))>10^-12
+         thetaN = thetaN - (B(thetaN)/Bder(thetaN));
+    end
+    tEvent = t(1)+thetaN*h;
+    yEvent = [B(thetaN); Bder(thetaN)/h];
 end
--- part2_bouncing_ball.m
+++ part2_bouncing_ball.m
@@ -10,19 +10,19 @@ t0=0;
 tend=100;
 Tol=10^-8;
 h0=10^-2;
-for bounces = 1:20
+for bounces = 1:40
     [t, y, iflag, nfun, njac] = RKs(f, jac, t0, tend, y0, Tol, h0,eventLocator);
     stop = find(y(1,:)<0);
     %find accurate t and y on impact:
-    [tImpact,yImpact,speedImpact] = hermiteInterpolation(t((stop-2):stop),y(:,(stop-2):stop));
+    [tEvent,yEvent] = hermiteInterpolation(t((stop-1):stop),y(:,(stop-1):stop));
     %y0 = [0;-0.9*y(2,stop-1)];
-    y0 = [0;-0.9*speedImpact];
+    y0 = [0;-0.9*yEvent(2)];
     if bounces == 1
-        ballY = [y(1,(1:stop-1)), yImpact];
-        ballT = [t(1:stop-1), tImpact];
+        ballY = [y(1,(1:stop-1)), yEvent(1)];
+        ballT = [t(1:stop-1), tEvent];
     else
-        ballY = [ballY, y(1,(1:stop-1)), yImpact];
-        ballT = [ballT, (ballT(end)+t(1:stop-1)), tImpact];
+        ballY = [ballY, y(1,(1:stop-1)), yEvent(1)];
+        ballT = [ballT, (ballT(end)+t(1:stop-1)), ballT(end)+tEvent];
     end 
 end
 plot(ballT,ballY)
