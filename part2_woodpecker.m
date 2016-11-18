6efc57c8e07c166e15baba21fffdba793aaf072c
sindrgro94 <sindre.grostad@icloud.com>
2016-11-18 09:39:14 +0100

    start woodpecker

---
+++ part2_woodpecker.m
@@ -0,0 +1,18 @@
+%Part 2 - The woodpecker toy problem
+clear all;
+close all;
+clc;
+a = 0.025; %m
+m1 = 0.0003; %kg
+d1 = 0.18335;
+Ok1 = 10; %degrees
+I2 = 7*10^-7; %kgm^2
+b = 0.015; %m
+m2 = 0.0045; %kg
+d2 = 0.04766;
+Ok2 = 12; %degrees
+c = 0.0056;%Nm
+g = 9.81; %m/s^2
+O0 = [
+%phase a, c, d:
+f1 = [
